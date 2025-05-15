e112db1511b681ba93e0a33dd932ae0142611b08
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-15 16:35:26 -0600

    build: first commit

---
+++ flags-tijuana-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace flags_tijuana_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
