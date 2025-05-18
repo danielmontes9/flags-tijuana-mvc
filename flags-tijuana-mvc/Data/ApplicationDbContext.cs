d5b0227be04b34623116bebdf10277fb6afc916d
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-17 18:47:20 -0600

    feat: add flag entity

--- flags-tijuana-mvc/Data/ApplicationDbContext.cs
+++ flags-tijuana-mvc/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using flags_tijuana_mvc.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace flags_tijuana_mvc.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<FlagEntity> Flags { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ flags-tijuana-mvc/Models/Entities/FlagEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace flags_tijuana_mvc.Models.Entities
+{
+    public class FlagEntity
+    {
+        [Key] public int Id { get; set; }
+        public required string Name { get; set; }
+        public IdentityUser User { get; set; } = null!;
+        public required virtual string UserId { get; set; }
+        public required bool Velue { get; set; }
+
+    }
+}
