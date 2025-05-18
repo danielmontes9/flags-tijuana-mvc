0320bddd9481f31f82441d72f4028d9778765d6e
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-17 22:02:44 -0600

    chore: add migration with add flag entity

--- flags-tijuana-mvc/Data/Migrations/20250518035711_AddFlagEntity.Designer.cs
+++ flags-tijuana-mvc/Data/Migrations/20250518035711_AddFlagEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using flags_tijuana_mvc.Data;
 
@@ -11,9 +12,11 @@ using flags_tijuana_mvc.Data;
 namespace flags_tijuana_mvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250518035711_AddFlagEntity")]
+    partial class AddFlagEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
