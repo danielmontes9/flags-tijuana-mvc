89e5ddff5aabbafb0ca7a3597f46020c77776bce
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-18 22:53:11 -0600

    feat: add use case AddFlag

---
+++ flags-tijuana-mvc/Controllers/UseCases/AddFlagUseCase.cs
@@ -0,0 +1,15 @@
+﻿using flags_tijuana_mvc.Data;
+
+namespace flags_tijuana_mvc.Controllers.UseCases
+{
+    public class AddFlagUseCase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public AddFlagUseCase(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+    }
+}
